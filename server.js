a0b503b619b6b5cc6cdbb1e4628cfa8f39071053
BradleyK <bradleyjkennett@gmail.com>
2022-06-29 13:24:07 -0400

    task create add

--- server.js
+++ server.js
@@ -12,39 +12,63 @@
 // WHEN I click on the Write icon in the navigation at the top of the page
 // THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
-const express = require('express')
-
-const { tasks } = require('./Develop/db/db.json')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { tasks } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
-const app = express()
+const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-function filterByQuery(query, taskArray) {
-    let filteredResults = taskArray;
-    if (query.title) {
-       filteredResults = filteredResults.filter(task => task.title === query.title);
-    }
+function findById(id, taskArray) {
+    const result = taskArray.filter(task => task.id === id)[0];
+    return result;
+}
 
-    if (query.text) {
-        filteredResults = filteredResults.filter(task => task.text === query.text);
-    }
+function createNewTask(body, taskArray){
+    const task = body;
+    taskArray.push(task);
+    fs.writeFileSync(
+        path.join(__dirname,'./Develop/db/db.json'),JSON.stringify({ tasks: taskArray}, null, 2));
+    return task;
+}
 
-    if (query.id) {
-        filteredResults = filteredResults.filter(task => task.id = query.id)
+function validateTask(task) {
+    if (!task.title || typeof task.title !== 'string') {
+        return false;
     }
-    return filteredResults;
+    if (!task.text || typeof task.text !== 'string') {
+        return false; 
+    }
+    return true;
 }
 
 app.get('/api/tasks', (req, res) => {
-    let results = tasks;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+
+    res.json(tasks);
+});
+
+app.get('/api/tasks/:id', (req, res) => {
+    const result = findById(req.params.id, tasks);
+    res.json(result);
+});
+
+app.post('/api/tasks', (req, res) => {
+    req.body.id = tasks.length.toString();
+
+    if (!validateTask(req.body)) {
+        res.status(400).send('The task is not properly formatted.');
+    } else {
+
+        const task = createNewTask(req.body, tasks);
+
+        res.json(task);
     }
-    res.json(results);
+
 });
 
 app.listen(PORT, () => {
